7cdf0338f0ad36d011240c323af4b964e03492d5
Pravetz <46265909+Pravetz@users.noreply.github.com>
2021-08-03 15:46:42 +0300

    Major update(version 0.2)
    - Added diagonal flags
    - Changed saving system
    - Coloring of heraldry

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int w = 300, h = 200;
 int flagtype;
 int heraldry;
 int hv;
-int save_int = 0;
+int colored_heraldry = 0;
 
 int main(int argc, char** argv)
 {
@@ -24,13 +24,14 @@ int main(int argc, char** argv)
 	srand(time(0));
 	flagtype = rand() % 3 + 1; /* 1 - monocolor, 2 - bicolor, 3 - tricolor  */
 	heraldry = rand() % 11; /* CHANGE THIS LINE IF YOU ADDED OR REMOVED HERALDRY IN THE ASSETS/GFX */
-	hv = rand() % 2 + 1; /*1 - horizontal, 2 - vertical */
+	hv = rand() % 4 + 1; /*1 - horizontal, 2 - vertical, 3 - diagonal, 4 - reverse diagonal */
+	
 	
 	std::cout <<"[i] "<<flagtype<<" "<<heraldry<<" "<<hv<<std::endl;
 	
 	sf::RenderWindow app(sf::VideoMode(w, h, 32), "SFML Flag generation tool by Pravetz");
 	
-	save_int = atoi(argv[1]);
+	colored_heraldry = atoi(argv[1]);
 	
 	sf::RenderTexture texture;
 	
@@ -43,21 +44,31 @@ int main(int argc, char** argv)
 	
 	Texture sector2;
 	
+	
 	sector.loadFromFile("assets/gfx/sectr.png");
 	sector2.loadFromFile("assets/gfx/sectr2.png");
 	
 	Texture nsector;
 	Texture nsector2;
+	Texture dsector;
+	Texture dsector2;
+	Texture csector;
+	Texture csector2;
 	
 	nsector.loadFromFile("assets/gfx/nsectr.png");
 	nsector2.loadFromFile("assets/gfx/nsectr2.png");
+	dsector.loadFromFile("assets/gfx/dsectr.png");
+	dsector2.loadFromFile("assets/gfx/dsectr2.png");
+	csector.loadFromFile("assets/gfx/csectr.png");
+	csector2.loadFromFile("assets/gfx/csectr2.png");
+	
 	
 	Sprite ns(nsector);
 	Sprite ns2(nsector2);
 	
-	Sprite s(sector);
-	Sprite s1(sector);
-	Sprite s2(sector);
+	Sprite s;
+	Sprite s1;
+	Sprite s2;
 	
 	Sprite s3(sector2);	
 	Sprite s4(sector2);
@@ -76,9 +87,74 @@ int main(int argc, char** argv)
 	///////////////////// POSITIONS /////////////////////////
 	
 	if (flagtype == 3 && hv == 2 && heraldry != 9 && heraldry != 8){		// TRICOLORS
+		s.setTexture(sector);			//
+		s1.setTexture(sector);			//
+		s2.setTexture(sector);			//
 		s.setPosition(0,0);				//
 		s1.setPosition(100,0);			//
 		s2.setPosition(200,0);			//
+	}
+	
+	if (flagtype == 3 && hv == 3 && heraldry != 8 && heraldry != 9){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(dsector2);		//
+		s2.setTexture(dsector);			//
+		s2.rotate(180.f);				//
+		s.setPosition(0,0);				//
+		s2.setPosition(300,200);		//
+	}
+	
+	if (flagtype == 3 && hv == 4 && heraldry != 8 && heraldry != 9){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(dsector2);		//
+		s2.setTexture(dsector);			//
+		s.setScale(-1,1);				//
+		s2.setScale(-1,1);				//
+		s1.setScale(-1,1);				//
+		s2.rotate(180.f);				//
+		s.setPosition(300,0);			//
+		s1.setPosition(300,0);			//
+		s2.setPosition(0,200);			//
+	}
+	
+	if (flagtype == 3 && hv == 3 && heraldry == 8){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(csector);			//
+		s2.setTexture(dsector);			//
+		s2.rotate(180.f);				//
+		s.setPosition(0,0);				//
+		s2.setPosition(300,200);		//
+	}
+	
+	if (flagtype == 3 && hv == 3 && heraldry == 9){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(csector2);		//
+		s2.setTexture(dsector);			//
+		s2.rotate(180.f);				//
+		s.setPosition(0,0);				//
+		s2.setPosition(300,200);		//
+	}
+	
+	if (flagtype == 3 && hv == 4 && heraldry == 8){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(csector);			//
+		s2.setTexture(dsector);			//
+		s.setScale(-1,1);				//
+		s2.setScale(-1,1);				//
+		s2.rotate(180.f);				//
+		s.setPosition(300,0);			//
+		s2.setPosition(0,200);			//
+	}
+	
+	if (flagtype == 3 && hv == 4 && heraldry == 9){		// DIAGONAL TRICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(csector2);		//
+		s2.setTexture(dsector);			//
+		s.setScale(-1,1);				//
+		s2.setScale(-1,1);				//
+		s2.rotate(180.f);				//
+		s.setPosition(300,0);			//
+		s2.setPosition(0,200);			//
 	}
 	
 	if (flagtype == 3 && hv == 1 && heraldry != 9 && heraldry != 8){		//
@@ -88,6 +164,8 @@ int main(int argc, char** argv)
 	}
 	
 	if (flagtype == 2 && hv == 2){		// BICOLORS
+		s.setTexture(sector);			//
+		s1.setTexture(sector);			//
 		s.setPosition(0,0);				//
 		s1.setPosition(150,0);			//
 	}
@@ -95,6 +173,24 @@ int main(int argc, char** argv)
 	if (flagtype == 2 && hv == 1){		//
 		s3.setPosition(0,0);			//
 		s4.setPosition(0,100);			//
+	}
+	
+	if (flagtype == 2 && hv == 3){		// DIAGONAL BICOLORS
+		s.setTexture(dsector);			//
+		s1.setTexture(dsector);			//
+		s1.rotate(180.f);				//
+		s.setPosition(0,0);				//
+		s1.setPosition(300,200);		//
+	}
+	
+	if (flagtype == 2 && hv == 4){		//
+		s.setTexture(dsector);			//
+		s1.setTexture(dsector);			//
+		s.setScale(-1,1);				//
+		s1.setScale(-1,1);				//
+		s1.rotate(180.f);				//
+		s.setPosition(300,0);			//
+		s1.setPosition(0,200);			//
 	}
 	
 	
@@ -139,6 +235,8 @@ int main(int argc, char** argv)
 	
 	if (flagtype == 3 && hv == 2 && heraldry == 9){ 	// SPECIAL TRICOLOR SCALING
 		sf::Vector2u size_flag = sector.getSize();		// FOR NORDIC CROSS ELEMENTS("9.png", "8.png")
+		s.setTexture(sector);
+		s2.setTexture(sector);
 		
 		float scale_x = size_flag.x * 2;
 		s2.setScale(scale_x, 1);
@@ -147,6 +245,8 @@ int main(int argc, char** argv)
 	
 	if (flagtype == 3 && hv == 2 && heraldry == 8){ 	// SPECIAL TRICOLOR SCALING
 		sf::Vector2u size_flag = sector.getSize();		// FOR NORDIC CROSS ELEMENTS("9.png", "8.png")
+		s.setTexture(sector);
+		s2.setTexture(sector);
 		
 		float scale_x = size_flag.x / 2;
 		s.setScale(scale_x, 1);
@@ -181,6 +281,15 @@ int main(int argc, char** argv)
 	///////////////////////////////////////////////////////////////
 	
 	if(flagtype == 3 && hv == 2 && heraldry != 8 && heraldry != 9){
+		s.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
+			std::cout<<"[i] stripe 1: "<<r<<" "<<g<<" "<<b<<endl;
+		s1.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
+			std::cout<<"[i] stripe 2: "<<r<<" "<<g<<" "<<b<<endl;
+		s2.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
+			std::cout<<"[i] stripe 3: "<<r<<" "<<g<<" "<<b<<endl;
+	}
+	
+	if((flagtype == 3 && hv == 3) || (flagtype == 3 && hv == 4)){
 		s.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
 			std::cout<<"[i] stripe 1: "<<r<<" "<<g<<" "<<b<<endl;
 		s1.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
@@ -238,7 +347,7 @@ int main(int argc, char** argv)
 	//							BICOLORS						 //
 	///////////////////////////////////////////////////////////////
 	
-	if(flagtype == 2 && hv == 2){
+	if((flagtype == 2 && hv == 2) || (flagtype == 2 && hv == 3) || (flagtype == 2 && hv == 4)){
 		s.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
 			std::cout<<"[i] stripe 1: "<<r<<" "<<g<<" "<<b<<endl;
 		s1.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
@@ -261,6 +370,15 @@ int main(int argc, char** argv)
 			std::cout<<"[i] color: "<<r<<" "<<g<<" "<<b<<endl;
 	}
 	
+	///////////////////////////////////////////////////////////////
+	//							HERALDRY						 //
+	///////////////////////////////////////////////////////////////
+	
+	if(heraldry > 0 && colored_heraldry == 1){
+		fl_e.setColor(sf::Color( r = rand() % 256, g = rand() % 256, b = rand() % 256));
+			std::cout<<"[i] heraldry color: "<<r<<" "<<g<<" "<<b<<endl;
+	}
+	
 	while (app.isOpen())
     {
         sf::Event event;
@@ -277,6 +395,12 @@ int main(int argc, char** argv)
 		texture.draw(s);
 		texture.draw(s1);
 		texture.draw(s2);
+	}
+	
+	if (flagtype == 3 && (hv == 3 || hv == 4)){
+		texture.draw(s);
+		texture.draw(s2);
+		texture.draw(s1);
 	}
 	
 	if (flagtype == 3 && hv == 2 && heraldry == 8){
@@ -309,7 +433,7 @@ int main(int argc, char** argv)
 		texture.draw(ns);
 	}
 	
-	if (flagtype == 2 && hv == 2){
+	if (flagtype == 2 && (hv == 2 || hv == 3 || hv == 4)){
 		texture.draw(s);
 		texture.draw(s1);
 	}
@@ -329,10 +453,11 @@ int main(int argc, char** argv)
 	
 	texture.display();
 	
-	std::string SAVED_FILENAME = std::to_string(flagtype)+" "+std::to_string(heraldry)+" "+std::to_string(hv)+".png";
+	std::string SAVED_FILENAME = std::to_string(flagtype)+" "+std::to_string(heraldry)+" "+std::to_string(hv)+" "+std::to_string(r+g+b)+".png";
 	
-	if(save_int == 1){
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
 		texture.getTexture().copyToImage().saveToFile(SAVED_FILENAME);
+		std::cout <<"[^] Saved flag."<<std::endl;
 	}
 	
 	app.clear(sf::Color(220,220,220));
@@ -342,10 +467,6 @@ int main(int argc, char** argv)
     
     app.draw(sprite);
     app.display();
-        }
-    
-    if(save_int == 1){
-		std::cout <<"[^] Saved flag."<<std::endl;
 	}
     
 	return 0;
